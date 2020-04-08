cc80b9f3073a2ee45630ab7357bf5ab06dfb2841
UzuLaura <58941369+UzuLaura@users.noreply.github.com>
2020-04-08 19:10:05 +0300

    separated addToTable function, fixed view link

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,26 +1,31 @@
+function addToTable(book) {
+    const table = document.querySelector('table tbody');
+
+    const tr = table.insertRow();
+
+    const td1 = tr.insertCell();
+    td1.textContent = book.name;
+
+    const td2 = tr.insertCell();    
+    td2.textContent = book.author;
+
+    const td3 = tr.insertCell();    
+    const link = document.createElement('a');
+    link.href = `view.html?${book.id}`;
+    link.textContent = 'View';
+    td3.append(link);
+}
+
 fetch("https://europe-west1-codeacademy-demo-f866c.cloudfunctions.net/books")
     .then((data) => data.json())
     .then((data) => {
-        const table = document.querySelector('table tbody');
         data.forEach((item) => {
             const book = {
                 name: item.name,
                 author: item.author,
                 id: item.id
             }
-            const tr = table.insertRow();
-
-            const td1 = tr.insertCell();
-            td1.textContent = book.name;
-
-            const td2 = tr.insertCell();    
-            td2.textContent = book.author;
-
-            const td3 = tr.insertCell();    
-            const link = document.createElement('a');
-            link.href = book.id;
-            link.textContent = 'View';
-            td3.append(link);
+            addToTable(book);
         });
     })
 
