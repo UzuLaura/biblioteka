9579d1a1a94a9e69b9d01481c3225e7ee86babe8
UzuLaura <58941369+UzuLaura@users.noreply.github.com>
2020-04-08 14:06:22 +0300

    added primitive notification message

--- assets/js/add.js
+++ assets/js/add.js
@@ -6,14 +6,21 @@ function captcha () {
     const captchaLabel = document.querySelector('label[for=captcha]');    
     captchaLabel.textContent += `${num1} + ${num2}`;    
     const form = document.querySelector('form');
+    const notification = document.querySelector('.notification');
 
     form.addEventListener('submit', e => {
         e.preventDefault();
 
-        if (captchaInput.value == sum) {
+        if (Number(captchaInput.value) == sum) {
             console.log('passed');
+            notification.textContent = "Added successfully";
+            notification.classList.add('bg-success');
+            notification.classList.add('display');
         } else {
             console.log('failed');
+            notification.textContent = "Check your captcha";
+            notification.classList.remove('bg-success');
+            notification.classList.add('display');
         }    
     })
 }
