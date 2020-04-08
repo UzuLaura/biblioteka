d8a35f55f5861de26d3fac3e2389c6df8c4eb2b0
UzuLaura <58941369+UzuLaura@users.noreply.github.com>
2020-04-08 12:04:28 +0300

    added primitive functionality to captcha function on submit

--- assets/js/add.js
+++ assets/js/add.js
@@ -1,9 +1,21 @@
 function captcha () {
     const num1 = Math.floor(Math.random() * 20 +1);
     const num2 = Math.floor(Math.random() * 20 +1);
-    
+    const sum = num1 + num2;
+    const captchaInput = document.getElementById('captcha');
     const captchaLabel = document.querySelector('label[for=captcha]');    
     captchaLabel.textContent += `${num1} + ${num2}`;    
+    const form = document.querySelector('form');
+
+    form.addEventListener('submit', e => {
+        e.preventDefault();
+
+        if (captchaInput.value == sum) {
+            console.log('passed');
+        } else {
+            console.log('failed');
+        }    
+    })
 }
 
 captcha();
