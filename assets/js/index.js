2b44f596e5f3603a6c84f55dc04884a921113e52
UzuLaura <58941369+UzuLaura@users.noreply.github.com>
2020-04-08 18:53:28 +0300

    added view to table

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,13 +5,22 @@ fetch("https://europe-west1-codeacademy-demo-f866c.cloudfunctions.net/books")
         data.forEach((item) => {
             const book = {
                 name: item.name,
-                author: item.author
+                author: item.author,
+                id: item.id
             }
             const tr = table.insertRow();
+
             const td1 = tr.insertCell();
-            const td2 = tr.insertCell();    
             td1.textContent = book.name;
+
+            const td2 = tr.insertCell();    
             td2.textContent = book.author;
+
+            const td3 = tr.insertCell();    
+            const link = document.createElement('a');
+            link.href = book.id;
+            link.textContent = 'View';
+            td3.append(link);
         });
     })
 
